606f695bb86e5b965de2d413b833ccf18e54eb21
Edwin Chiu <echiu2@buffalo.edu>
2020-07-01 20:27:15 -0400

    zerg rush create extractor and spawning pool

--- Starcraft II/bots/zerg/zerg_rush.py
+++ Starcraft II/bots/zerg/zerg_rush.py
@@ -2,14 +2,20 @@ import sc2
 import random
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import LARVA, DRONE, OVERLORD, HATCHERY, EXTRACTOR, QUEEN
+from sc2.constants import LARVA, DRONE, OVERLORD, HATCHERY, EXTRACTOR, QUEEN, SPAWNINGPOOL
 
 class ZergRush(sc2.BotAI):
+
+    def __init__(self):
+        self.gas_workers = None
+
     async def on_step(self, iteration: int):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_extractor()
+        await self.get_gas()
         await self.build_supply()
+        await self.build_offensive_force()
         await self.build_queen()
 
     async def build_workers(self):
@@ -24,14 +30,32 @@ class ZergRush(sc2.BotAI):
                 if self.can_afford(OVERLORD):
                     self.train(OVERLORD, 1)
 
+    async def build_offensive_force(self):
+        if not self.structures(SPAWNINGPOOL) and not self.already_pending(SPAWNINGPOOL):
+            if self.can_afford(SPAWNINGPOOL):
+                hatchery = self.townhalls[0]
+                for d in range(4, 15):
+                    pos = hatchery.position.towards(self.game_info.map_center, d)
+                    drone = self.workers.closest_to(pos)
+                    self.do(drone.build(SPAWNINGPOOL, pos))
+
+
+    async def get_gas(self):
+        if self.structures(EXTRACTOR).ready.exists and not self.gas_workers:
+            extractor = self.gas_buildings.first
+            self.gas_workers = self.workers.random_group_of(3)
+            for drone in self.gas_workers:
+                if extractor:
+                    self.do(drone.gather(target=extractor, queue=True))
+
     async def build_queen(self):
-        if not self.already_pending(QUEEN) and self.units(QUEEN).amount <= 2:
-            hatchery = self.townhalls(HATCHERY).exists
-            if self.can_afford(QUEEN) and hatchery:
-                self.do(hatchery.train(QUEEN))
+        if self.structures(SPAWNINGPOOL).ready.exists:
+            if not self.already_pending(QUEEN) and self.units(QUEEN).amount <= 2:
+                if self.can_afford(QUEEN):
+                    self.train(QUEEN)
 
     async def build_extractor(self):
-        if not self.structures(EXTRACTOR) and self.structures(EXTRACTOR).amount < 1:
+        if not self.structures(EXTRACTOR) or self.structures(EXTRACTOR).amount <= 1:
             if self.can_afford(EXTRACTOR):
                 drone = self.workers.random
                 target = self.vespene_geyser.closest_to(drone)
